63bf242929201b0e96959e38038d13fcf97e1ab8
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-28 23:50:51 +0600

    AllNames

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
+import AllNames from "./components/AllNames";
+import User from "./components/User";
 
 export default function App() {
   const [users, setUsers] = useState([]);
+  const [single, setSingle] = useState({});
   const [loading, setLoading] = useState(false);
+
+  const setData = (f, l) => {
+    const singleUser = users.find(
+      (user) => user.profile.firstName === f && user.profile.lastName === l
+    );
+    setSingle(singleUser);
+  };
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -22,8 +32,31 @@ export default function App() {
   console.log(users);
 
   return (
-    <div className="bg-red-500">
-     
+    <div className="">
+      <section className="flex md:gap-x-[105px] gap-x-2 justify-center">
+        <div className="w-40 md:w-[623px] md:ml-[105px] mt-20">
+          <h1 className="py-[21px] bg-[#C5DFFF] text-center font-[500] rd">
+            USERS LIST
+          </h1>
+
+          {users &&
+            users.map((user, index) => (
+              <AllNames
+                key={index}
+                user={user}
+                setData={setData}
+                single={single}
+              />
+            ))}
+        </div>
+        <div className="md:w-[502px] md:mr-[105px] mt-20">
+          <h1 className="py-[21px] bg-[#C5DFFF] text-center font-[500] rd">
+            USERS LIST
+          </h1>
+
+          <User single={single} />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/AllNames.jsx
+++ src/components/AllNames.jsx
@@ -1,5 +1,27 @@
-export default function AllNames() {
+/* eslint-disable react/prop-types */
+import avatar from "../assets/avatar.png";
+
+export default function AllNames({ user, setData, single }) {
+  const isActive = single.jobTitle === user.jobTitle;
   return (
-    <div>AllNames</div>
-  )
+    <div
+      onClick={() => setData(user.profile.firstName, user.profile.lastName)}
+      className={`my-[10px] rounded ${
+        isActive ? "bg-[#D4D4D4]" : "bg-[#ECECEC]"
+      }`}
+    >
+      <div className="flex items-center px-1 md:px-2 ">
+        <img
+          src={avatar}
+          alt="avatar"
+          width={52}
+          height={50}
+          className="rounded-full w-5 h-5 md:h-[50px] md:w-[52px]"
+        />
+        <h1 className="text-sm cursor-pointer p-1.5 md:p-5 md:w-[623px] font-[500] py-3 md:py-[18.5px] md:text-2xl">
+          {user.profile.firstName + " " + user.profile.lastName}
+        </h1>
+      </div>
+    </div>
+  );
 }
