c1184d2fc6494247455c5f1c39b1d865f9b65ad8
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-28 22:42:34 +0600

    data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setLoading(true);
+      const response = await fetch(
+        "https://602e7c2c4410730017c50b9d.mockapi.io/users"
+      );
+      const data = await response.json();
+      if (data.length > 0) {
+        setLoading(false);
+      }
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
+  console.log(users);
+
+  return <div className="bg-red-500">App</div>;
 }
